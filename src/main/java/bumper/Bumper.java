7e04f262f538a0d70354fb40290ab7404bb005da
Jake Robers <jakerobers@gmail.com>
2016-10-17 20:34:32 -0500

    Renamed to bumper

--- src/main/java/bumper/Bumper.java
+++ src/main/java/bumper/Bumper.java
@@ -1,4 +1,4 @@
-package rsbumper;
+package bumper;
 
 import java.util.concurrent.TimeUnit;
 
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class RSBumper {
+public class Bumper {
 	public static void main(String args[]) {
 
 		String username = null;
